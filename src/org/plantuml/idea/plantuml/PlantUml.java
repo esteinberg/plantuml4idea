cf95a18c568a278a6194bf8788a888d1c6d4555a
mamontov <Rgg60fbd>
2012-06-26 00:22:50 -0700

    Code cleanup. Tooltip was added.

--- src/org/plantuml/idea/plantuml/PlantUml.java
+++ src/org/plantuml/idea/plantuml/PlantUml.java
@@ -6,7 +6,6 @@ import net.sourceforge.plantuml.SourceStringReader;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author Eugene Steinberg
@@ -32,47 +31,28 @@ public class PlantUml {
                 return FileFormat.EPS;
             }
         };
+
         abstract FileFormat getFormat();
     }
 
     public static final String STARTUML = "@startuml";
 
-    public static PlantUmlResult render (String source) {
-        return render (source, ImageFormat.PNG);
+    public static PlantUmlResult render(String source) {
+        return render(source, ImageFormat.PNG);
     }
+
     public static PlantUmlResult render(String source, ImageFormat format) {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        InputStream is = null;
         String desc = null;
         String error = null;
         try {
             // image generation.
             SourceStringReader reader = new SourceStringReader(source);
             // Write the image to "os"
-
             desc = reader.generateImage(os, new FileFormatOption(format.getFormat()));
-            // close all the flow.
-            os.flush();
-
         } catch (IOException e) {
             error = e.getMessage();
-        } finally {
-            try {
-                os.close();
-            } catch (IOException e) {
-                // do nothing
-            }
         }
         return new PlantUmlResult(os.toByteArray(), desc, error);
-    }
-
-    /**
-     * Checks wherever content contains PlantUML source code
-     *
-     * @param content input content
-     * @return true if content is a PlantUML source
-     */
-    public static boolean containsPlantUmlSource(String content) {
-        return content.contains(STARTUML);
     }
 }
