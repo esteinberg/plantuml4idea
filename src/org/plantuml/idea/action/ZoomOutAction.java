4fef8e23ff19293246150859c9604247af20efe9
Eugene Steinberg <eugene.steinberg@gmail.com>
2012-09-05 18:07:24 -0700

    Boilerplate for zoom functionality

---
+++ src/org/plantuml/idea/action/ZoomOutAction.java
@@ -0,0 +1,15 @@
+package org.plantuml.idea.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author Eugene Steinberg
+ */
+public class ZoomOutAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: implement me
+
+    }
+}
