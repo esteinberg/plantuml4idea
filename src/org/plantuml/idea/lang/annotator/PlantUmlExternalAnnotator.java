ff1f906b20f88482fa0c44b2f31e25365ddddd0f
Eugene Steinberg <eugene.steinberg@gmail.com>
2014-09-27 12:47:12 -0700

    This closes #68 support for intentions.

--- src/org/plantuml/idea/lang/annotator/PlantUmlExternalAnnotator.java
+++ src/org/plantuml/idea/lang/annotator/PlantUmlExternalAnnotator.java
@@ -1,12 +1,17 @@
 package org.plantuml.idea.lang.annotator;
 
 import com.google.common.base.Joiner;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.ExternalAnnotator;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
 import net.sourceforge.plantuml.syntax.SyntaxChecker;
 import net.sourceforge.plantuml.syntax.SyntaxResult;
 import org.jetbrains.annotations.NotNull;
@@ -15,13 +20,15 @@ import org.plantuml.idea.lang.settings.PlantUmlSettings;
 import org.plantuml.idea.plantuml.PlantUml;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Author: Eugene Steinberg
  * Date: 9/13/14
  */
-public class PlantUmlExternalAnnotator extends ExternalAnnotator<PsiFile, Map<Integer, AnnotationResult>> {
+public class PlantUmlExternalAnnotator extends ExternalAnnotator<PsiFile, Map<Integer, SyntaxResult>> {
     @Nullable
     @Override
     public PsiFile collectInformation(@NotNull PsiFile file) {
@@ -30,14 +37,14 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<PsiFile, Map<In
 
     @Nullable
     @Override
-    public Map<Integer, AnnotationResult> doAnnotate(PsiFile collectedInfo) {
-        Map<Integer, AnnotationResult> result = new LinkedHashMap<Integer, AnnotationResult>();
+    public Map<Integer, SyntaxResult> doAnnotate(PsiFile collectedInfo) {
+        Map<Integer, SyntaxResult> result = new LinkedHashMap<Integer, SyntaxResult>();
         if (PlantUmlSettings.getInstance().isErrorAnnotationEnabled()) {
             String text = collectedInfo.getFirstChild().getText();
             Map<Integer, String> sources = PlantUml.extractSources(text);
             for (Map.Entry<Integer, String> sourceData : sources.entrySet()) {
-                AnnotationResult annotationResult = getAnnotationResult(sourceData.getValue());
-                if (annotationResult != null) {
+                SyntaxResult annotationResult = getAnnotationResult(sourceData.getValue());
+                if (annotationResult.isError()) {
                     result.put(sourceData.getKey(), annotationResult);
                 }
             }
@@ -47,23 +54,16 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<PsiFile, Map<In
         return result;
     }
 
-    private AnnotationResult getAnnotationResult(String source) {
-        AnnotationResult result = null;
-        SyntaxResult syntaxResult = SyntaxChecker.checkSyntax(source);
-        if (syntaxResult.isError()) {
-            String error = Joiner.on(" \n").join(syntaxResult.getErrors());
-            String suggest = Joiner.on(" \n").join(syntaxResult.getSuggest());
-            result = new AnnotationResult(error, suggest, syntaxResult.getErrorLinePosition());
-        }
-        return result;
+    private SyntaxResult getAnnotationResult(String source) {
+        return SyntaxChecker.checkSyntax(source);
     }
 
     @Override
-    public void apply(@NotNull PsiFile file, Map<Integer, AnnotationResult> annotationResult, @NotNull AnnotationHolder holder) {
+    public void apply(@NotNull PsiFile file, Map<Integer, SyntaxResult> annotationResult, @NotNull AnnotationHolder holder) {
         if (null != annotationResult) {
             Document document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
             if (document != null) {
-                for (Map.Entry<Integer, AnnotationResult> arEntry : annotationResult.entrySet()) {
+                for (Map.Entry<Integer, SyntaxResult> arEntry : annotationResult.entrySet()) {
                     annotateSource(arEntry.getValue(), holder, document, arEntry.getKey());
                 }
 
@@ -71,12 +71,63 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<PsiFile, Map<In
         }
     }
 
-    private void annotateSource(AnnotationResult annotationResult, AnnotationHolder holder, Document document, int baseOffset) {
+    private void annotateSource(SyntaxResult annotationResult, AnnotationHolder holder, Document document, int baseOffset) {
         int sourceStartLineNumber = document.getLineNumber(baseOffset);
-        int startoffset = document.getLineStartOffset(annotationResult.getLineNumber() + sourceStartLineNumber);
-        int endoffset = document.getLineEndOffset(annotationResult.getLineNumber() + sourceStartLineNumber);
+        int startoffset = document.getLineStartOffset(annotationResult.getErrorLinePosition() + sourceStartLineNumber);
+        int endoffset = document.getLineEndOffset(annotationResult.getErrorLinePosition() + sourceStartLineNumber);
         TextRange range = TextRange.create(startoffset, endoffset);
-        holder.createErrorAnnotation(range,
-                annotationResult.getErrorMessage() + " " + annotationResult.getSuggestion());
+        String errorMessage = Joiner.on("\n").join(annotationResult.getErrors());
+        Annotation errorAnnotation = holder.createErrorAnnotation(range,
+                errorMessage);
+        for (String s : cleanupSuggestions(annotationResult.getSuggest())) {
+            errorAnnotation.registerFix(new PlantUmlIntentionAction(startoffset, endoffset, s));
+        }
+
+
+    }
+
+    private List<String> cleanupSuggestions(List<String> suggest) {
+        LinkedList<String> suggestions = new LinkedList<String>(suggest);
+        suggestions.remove("Did you mean:");
+        return suggestions;
+    }
+
+    private class PlantUmlIntentionAction implements IntentionAction {
+        private int startOffset;
+        private int endOffset;
+        private String suggestion;
+
+        protected PlantUmlIntentionAction(int startOffset, int endOffset, String suggestion) {
+            this.startOffset = startOffset;
+            this.endOffset = endOffset;
+            this.suggestion = suggestion;
+        }
+
+        @NotNull
+        @Override
+        public String getText() {
+            return "change to '" + suggestion + "'";
+        }
+
+        @NotNull
+        @Override
+        public String getFamilyName() {
+            return "PLANTUML_INTENTIONS";
+        }
+
+        @Override
+        public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+            return true;
+        }
+
+        @Override
+        public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+            editor.getDocument().replaceString(startOffset, endOffset, suggestion);
+        }
+
+        @Override
+        public boolean startInWriteAction() {
+            return true;
+        }
     }
 }
