196168c3592c0d3eaa59c7fc9fd795e04c49d9b2
Vojtech Krasa <vojta.krasa@gmail.com>
2014-12-09 18:41:23 +0100

    ToolWindow usable during indexing

--- src/org/plantuml/idea/toolwindow/PlantUmlToolWindowFactory.java
+++ src/org/plantuml/idea/toolwindow/PlantUmlToolWindowFactory.java
@@ -1,6 +1,6 @@
 package org.plantuml.idea.toolwindow;
 
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.*;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
@@ -9,7 +9,7 @@ import com.intellij.ui.content.ContentFactory;
 /**
  * @author Eugene Steinberg
  */
-public class PlantUmlToolWindowFactory implements ToolWindowFactory {
+public class PlantUmlToolWindowFactory implements ToolWindowFactory, DumbAware {
 
     public static final String ID = "PlantUML";
 
