dce66ba55654bac414dda862e8b937d622a3f053
Vojtech Krasa <vojta.krasa@gmail.com>
2015-06-28 00:27:32 +0200

    Browse button in settings

--- src/org/plantuml/idea/lang/settings/PlantUmlSettingsPage.java
+++ src/org/plantuml/idea/lang/settings/PlantUmlSettingsPage.java
@@ -1,11 +1,20 @@
 package org.plantuml.idea.lang.settings;
 
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Max Gorbunov
@@ -14,6 +23,31 @@ public class PlantUmlSettingsPage implements Configurable {
     private JPanel panel;
     private JTextField textFieldDotExecutable;
     private JCheckBox plantUMLErrorAnnotationExperimentalCheckBox;
+    private JButton browse;
+    private Project project;
+
+    public PlantUmlSettingsPage(Project project) {
+        this.project = project;
+        browse.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                browseForFile(textFieldDotExecutable);
+            }
+        });
+    }
+
+    private void browseForFile(@NotNull final JTextField target) {
+        final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor();
+        descriptor.setTitle("Select path to Graphviz/DOT executable (dot.exe)");
+        String text = target.getText();
+        final VirtualFile toSelect = text == null || text.isEmpty() ? project.getBaseDir()
+                : LocalFileSystem.getInstance().findFileByPath(text);
+
+        // 10.5 does not have #chooseFile
+        VirtualFile virtualFile = FileChooser.chooseFile(descriptor, project, toSelect);
+        if (virtualFile != null) {
+            target.setText(virtualFile.getPath());
+        }
+    }
 
     @Nls
     @Override
