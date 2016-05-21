b177ba4a344849d8dce7ae3dd60046a763d48d8c
Vojtech Krasa <vojta.krasa@gmail.com>
2016-05-22 00:52:54 +0200

    skip console events

--- src/org/plantuml/idea/toolwindow/listener/PlantUmlCaretListener.java
+++ src/org/plantuml/idea/toolwindow/listener/PlantUmlCaretListener.java
@@ -3,20 +3,28 @@ package org.plantuml.idea.toolwindow.listener;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.event.CaretEvent;
 import com.intellij.openapi.editor.event.CaretListener;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import org.plantuml.idea.util.LazyApplicationPoolExecutor;
 import org.plantuml.idea.util.UIUtils;
 
 public class PlantUmlCaretListener implements CaretListener {
     private static Logger logger = Logger.getInstance(PlantUmlCaretListener.class);
+    public FileDocumentManager instance = FileDocumentManager.getInstance();
 
     @Override
     public void caretPositionChanged(final CaretEvent e) {
+        if (instance.getFile(e.getEditor().getDocument()) == null) {
+            return;//console            
+        }
         logger.debug("caretPositionChanged");
         UIUtils.renderPlantUmlToolWindowLater(e.getEditor().getProject(), LazyApplicationPoolExecutor.Delay.POST_DELAY);
     }
 
     @Override
     public void caretAdded(CaretEvent e) {
+        if (instance.getFile(e.getEditor().getDocument()) == null) {
+            return;//console            
+        }
         logger.debug("caretAdded");
         UIUtils.renderPlantUmlToolWindowLater(e.getEditor().getProject(), LazyApplicationPoolExecutor.Delay.POST_DELAY);
     }
--- src/org/plantuml/idea/toolwindow/listener/PlantUmlDocumentListener.java
+++ src/org/plantuml/idea/toolwindow/listener/PlantUmlDocumentListener.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.plantuml.idea.util.LazyApplicationPoolExecutor;
 import org.plantuml.idea.util.UIUtils;
 
@@ -12,6 +14,7 @@ import static com.intellij.codeInsight.completion.CompletionInitializationContex
 
 public class PlantUmlDocumentListener implements DocumentListener {
     private static Logger logger = Logger.getInstance(PlantUmlDocumentListener.class);
+    public FileDocumentManager instance = FileDocumentManager.getInstance();
 
     @Override
     public void beforeDocumentChange(DocumentEvent event) {
@@ -19,9 +22,14 @@ public class PlantUmlDocumentListener implements DocumentListener {
 
     @Override
     public void documentChanged(DocumentEvent event) {
-        if (logger.isDebugEnabled()) {
-            logger.debug("document changed ");
+        VirtualFile file = instance.getFile(event.getDocument());
+        if (file == null) {
+            return;//console
         }
+        if (logger.isDebugEnabled()) {
+            logger.debug("document changed ", event.getSource());
+        }
+
         //#18 Strange "IntellijIdeaRulezzz" - filter code completion event.
         if (!DUMMY_IDENTIFIER.equals(event.getNewFragment().toString())) {
             Editor[] editors = EditorFactory.getInstance().getEditors(event.getDocument());
@@ -31,3 +39,4 @@ public class PlantUmlDocumentListener implements DocumentListener {
         }
     }
 }
+
