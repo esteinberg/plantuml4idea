f5acc29f35911cc47a56162108b7bec7b93e564b
Vojtech Krasa <vojta.krasa@gmail.com>
2016-06-16 04:10:22 +0200

    Usage manual

--- src/org/plantuml/idea/toolwindow/Usage.java
+++ src/org/plantuml/idea/toolwindow/Usage.java
@@ -6,9 +6,9 @@ public class Usage extends JTextArea {
     public static final String TEXT =
             "- Open file, put the cursor inside of PlantUML code to render it\n"
                     + "- A new file can be created from template (File | New | select e.g. 'UML Sequence' template)\n"
-                    + "- Diagram code can be placed anywhere, '*' are ignored for usage in Java comments\n"
-                    + "- To render other than sequence diagram types, open settings and setup path to Graphviz\n"
-                    + "- About screen tests your installation\n"
+                    + "- PlantUML code can be placed anywhere, '*' are ignored for usage in Java comments\n"
+                    + "- To render other than sequence diagram types, install Graphviz and set path to it\n"
+                    + "- About screen tests Graphviz installation\n"
                     + "- You can disable automatic rendering and use shortcut Alt+D, or reload button in the toolbar\n"
                     + "- Image cache size can be tuned in the settings, if you have not enough heap.\n"
                     + "- You can use intention to reverse arrows (Alt+Enter)\n"
