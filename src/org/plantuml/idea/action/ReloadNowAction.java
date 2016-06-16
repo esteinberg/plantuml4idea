267524e7ac43250ae5d4afea74f3af01b80fca7d
Vojtech Krasa <vojta.krasa@gmail.com>
2016-06-16 17:10:01 +0200

    #125 color indicator of disabled rendering

--- src/org/plantuml/idea/action/ReloadNowAction.java
+++ src/org/plantuml/idea/action/ReloadNowAction.java
@@ -1,14 +1,22 @@
 package org.plantuml.idea.action;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import org.plantuml.idea.lang.settings.PlantUmlSettings;
 import org.plantuml.idea.rendering.LazyApplicationPoolExecutor;
 import org.plantuml.idea.rendering.RenderCommand;
 import org.plantuml.idea.toolwindow.PlantUmlToolWindow;
 import org.plantuml.idea.util.UIUtils;
 
 public class ReloadNowAction extends DumbAwareAction {
+
+    private PlantUmlSettings settings;
+
+    public ReloadNowAction() {
+        settings = PlantUmlSettings.getInstance();
+    }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -21,4 +29,15 @@ public class ReloadNowAction extends DumbAwareAction {
         }
     }
 
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        if (settings.isAutoRender()) {
+            e.getPresentation().setIcon(AllIcons.Actions.Refresh);
+            e.getPresentation().setDescription("Reload PlantUml Diagram");
+        } else {
+            e.getPresentation().setIcon(AllIcons.Actions.ForceRefresh);
+            e.getPresentation().setDescription("Reload PlantUml Diagram (Automatic Rendering Disabled)");
+        }
+    }
 }
