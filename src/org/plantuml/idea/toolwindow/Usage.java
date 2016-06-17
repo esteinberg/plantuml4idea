e27e5b7641da1f70cc808a88756b185ecb32c668
Vojtech Krasa <vojta.krasa@gmail.com>
2016-06-17 04:01:11 +0200

    usage update

--- src/org/plantuml/idea/toolwindow/Usage.java
+++ src/org/plantuml/idea/toolwindow/Usage.java
@@ -9,17 +9,16 @@ public class Usage extends JTextArea {
                     + "- PlantUML code can be placed anywhere, '*' are ignored for usage in Java comments\n"
                     + "- To render other than sequence diagram types, install Graphviz and set path to it\n"
                     + "- About screen tests Graphviz installation\n"
-                    + "- You can disable automatic rendering and use shortcut Alt+D, or reload button in the toolbar\n"
                     + "- Image cache size can be tuned in the settings, if you have not enough heap\n"
                     + "- You can use intention to reverse arrows (Alt+Enter)\n"
                     + "- Cursor on top of @startuml allows to use following intentions:\n"
                     + "  - disable syntax check\n"
                     + "  - enable partial rendering - renders each page on it's own, useful for big sequence diagram files\n"
                     + "- Performance tips:\n"
+                    + "  - disable automatic rendering and use Update (Alt+D) or Reload (Alt+F) buttons\n"
                     + "  - do not put @newpage into included files (it prohibits incremental rendering)\n"
                     + "  - try to enable partial rendering\n"
                     + "  - disable syntax checking\n"
-                    + "  - disable automatic rendering\n"
                     + "";
 
     public Usage() {
