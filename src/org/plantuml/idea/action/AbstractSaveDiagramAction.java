e911f2155d7e087360ff3d729ef4af294f5cf84f
Vojtěch Krása <vojta.krasa@gmail.com>
2019-06-01 04:42:35 +0200

    Fixes #225 Export diagram - missing extension

--- src/org/plantuml/idea/action/AbstractSaveDiagramAction.java
+++ src/org/plantuml/idea/action/AbstractSaveDiagramAction.java
@@ -101,7 +101,16 @@ public abstract class AbstractSaveDiagramAction extends DumbAwareAction {
                 File file = wrapper.getFile();
                 String[] tokens = file.getAbsolutePath().split("\\.(?=[^\\.]+$)");
                 String base = tokens[0];
-                String extension = tokens.length < 2 ? "png" : tokens[1];
+                String extension;
+
+                if (tokens.length < 2) {
+                    extension = "png";
+                    file = new File(base + ".png");
+                } else {
+                    extension = tokens[1];
+                }
+
+
                 PlantUml.ImageFormat imageFormat;
                 try {
                     imageFormat = PlantUml.ImageFormat.valueOf(extension.toUpperCase());
