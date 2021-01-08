219e9875ddf3272b19266d856fe48cace46ca61a
Vojtěch Krása <vojta.krasa@gmail.com>
2021-01-08 15:25:05 +0100

    cleanup

--- src/org/plantuml/idea/external/ParentLastURLClassLoader.java
+++ src/org/plantuml/idea/external/ParentLastURLClassLoader.java
@@ -87,9 +87,8 @@ public class ParentLastURLClassLoader extends ClassLoader {
 
     public ParentLastURLClassLoader(ClassLoader parent, URL... urls) {
         super(parent);
-        childClassLoader = new ChildURLClassLoader(urls,
-                new FindClassClassLoader(this.getParent()));
-        childClassLoader.setDefaultAssertionStatus(false);
+        childClassLoader = new ChildURLClassLoader(urls, new FindClassClassLoader(this.getParent()));
+        childClassLoader.setDefaultAssertionStatus(false);    //for performance
     }
 
     @Override
