a3c461986009929b12cdac76f8a200b18d02861f
Vojtěch Krása <vojta.krasa@gmail.com>
2021-01-08 15:10:41 +0100

    cleanup

--- src/org/plantuml/idea/action/ShowSettingsAction.java
+++ src/org/plantuml/idea/action/ShowSettingsAction.java
@@ -8,6 +8,6 @@ import org.plantuml.idea.lang.settings.PlantUmlSettingsPage;
 public class ShowSettingsAction extends DumbAwareAction {
     @Override
     public void actionPerformed(AnActionEvent event) {
-        ShowSettingsUtil.getInstance().editConfigurable(event.getProject(), "PlantUMLSettings", new PlantUmlSettingsPage());
+        ShowSettingsUtil.getInstance().editConfigurable(event.getProject(), "PlantUMLSettings2", new PlantUmlSettingsPage());
     }
 }
