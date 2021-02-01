c5005f3008a25bd788f4f617331d680e66227cd2
Vojtěch Krása <vojta.krasa@gmail.com>
2021-02-01 11:29:54 +0100

    DisplayLinksBorderToggleAction

---
+++ src/org/plantuml/idea/action/DisplayLinksBorderToggleAction.java
@@ -0,0 +1,23 @@
+package org.plantuml.idea.action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.project.DumbAware;
+import org.plantuml.idea.lang.settings.PlantUmlSettings;
+import org.plantuml.idea.rendering.LazyApplicationPoolExecutor;
+import org.plantuml.idea.rendering.RenderCommand;
+import org.plantuml.idea.util.UIUtils;
+
+public class DisplayLinksBorderToggleAction extends ToggleAction implements DumbAware {
+
+    @Override
+    public boolean isSelected(AnActionEvent anActionEvent) {
+        return PlantUmlSettings.getInstance().isShowUrlLinksBorder();
+    }
+
+    @Override
+    public void setSelected(AnActionEvent anActionEvent, boolean b) {
+        PlantUmlSettings.getInstance().setShowUrlLinksBorder(b);
+        UIUtils.renderPlantUmlToolWindowLater(getEventProject(anActionEvent), LazyApplicationPoolExecutor.Delay.NOW, RenderCommand.Reason.REFRESH);
+    }
+}
