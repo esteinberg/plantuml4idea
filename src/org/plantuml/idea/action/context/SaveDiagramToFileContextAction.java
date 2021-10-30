23018ac4df10f6f9a6fef5a711d0e2586e985023
Vojtěch Krása <vojta.krasa@gmail.com>
2021-10-30 17:53:28 +0200

    Fixes #329 missing icon

--- src/org/plantuml/idea/action/context/SaveDiagramToFileContextAction.java
+++ src/org/plantuml/idea/action/context/SaveDiagramToFileContextAction.java
@@ -1,14 +1,17 @@
 package org.plantuml.idea.action.context;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.util.IconLoader;
 import org.plantuml.idea.action.save.AbstractSaveDiagramAction;
 import org.plantuml.idea.preview.image.ImageContainer;
 
+import javax.swing.*;
+
 public class SaveDiagramToFileContextAction extends AbstractSaveDiagramAction {
+    public static final Icon ICON = IconLoader.getIcon("/images/menu-saveall.svg", SaveDiagramToFileContextAction.class);
 
     public SaveDiagramToFileContextAction() {
-        super("Save Diagram", null, AllIcons.Actions.MenuSaveall);
+        super("Save Diagram", null, ICON);
     }
 
     @Override
