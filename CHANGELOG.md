0624ffd4d98d6e8a33a8b965285fcc7d36ccf3b1
Vojtěch Krása <vojta.krasa@gmail.com>
2023-05-31 10:36:14 +0200

    Fixes #391 SaveAllDiagramToFileAction - invalid editor null

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,11 @@
 
 ## Unreleased
 
+- bugfix
+
 ## 6.1.0-IJ2022.2 - 2023-05-15
 
-- highlighting of functions and variables  [#389](https://github.com/esteinberg/plantuml4idea/issues/389)
+- highlighting of functions and variables #389
 - PlantUML library upgrade to v1.2023.7
 
 ## 6.0.0-IJ2022.2 - 2023-04-27
--- src/main/java/org/plantuml/idea/action/save/SaveAllDiagramToFileAction.java
+++ src/main/java/org/plantuml/idea/action/save/SaveAllDiagramToFileAction.java
@@ -1,17 +1,37 @@
 package org.plantuml.idea.action.save;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+import org.plantuml.idea.preview.PlantUmlPreviewPanel;
+import org.plantuml.idea.preview.editor.PlantUmlSplitEditor;
+import org.plantuml.idea.util.UIUtils;
 
 /**
  * @author Eugene Steinberg
  */
 public class SaveAllDiagramToFileAction extends AbstractSaveDiagramAction {
 
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-        final Project project = e.getProject();
-        e.getPresentation().setEnabled(project != null);
-    }
+	@Override
+	public void update(@NotNull AnActionEvent e) {
+		final Project project = e.getProject();
+		boolean enabled = false;
+		if (project != null) {
+			PlantUmlPreviewPanel previewPanel = UIUtils.getEditorOrToolWindowPreview(e);
+			if (previewPanel != null) {
+				if (previewPanel.getDisplayedItem() != null) {
+					enabled = true;
+				} else {  //maybe redundant
+					FileEditor editor = e.getData(PlatformDataKeys.FILE_EDITOR);
+					if (editor instanceof PlantUmlSplitEditor) {
+						enabled = true;
+					}
+				}
+			}
+		}
+
+		e.getPresentation().setEnabled(enabled);
+	}
 }
