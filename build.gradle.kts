bf4bf457f2a1302565700d23840c0fbb48d6d581
Vojtěch Krása <vojta.krasa@gmail.com>
2023-06-04 21:15:32 +0200

    gradle build changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.changelog.Changelog
+@file:Suppress("HardCodedStringLiteral")
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
@@ -8,11 +8,39 @@ plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin)
     alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
-    alias(libs.plugins.changelog) // Gradle Changelog Plugin
+//    alias(libs.plugins.changelog) // Gradle Changelog Plugin
 }
 
 group = properties("pluginGroup").get()
-version = properties("pluginVersion").get()
+
+version = "6.2.2-IJ2022.2"
+
+tasks {
+    patchPluginXml {
+        // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html
+        sinceBuild.set("222.4167.9")
+        untilBuild.set("")
+        changeNotes.set(
+            """
+            - IntelliJ 2023.2 compatibility
+            - support for @startdef
+            - bugfix
+            """.trimIndent()
+        )
+    }
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    pluginName = properties("pluginName")
+    //https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html#configuration-intellij-extension
+    version = "LATEST-EAP-SNAPSHOT"
+    type = "IC"
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
+
 
 // Configure project's dependencies
 repositories {
@@ -49,21 +77,12 @@ kotlin {
     jvmToolchain(17)
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
-
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
-}
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    groups.empty()
-//    repositoryUrl.set("https://github.com/esteinberg/plantuml4idea")
-}
+//changelog {
+//    groups.empty()
+////    repositoryUrl.set("https://github.com/esteinberg/plantuml4idea")
+//}
 
 
 tasks {
@@ -75,37 +94,6 @@ tasks {
         enabled = false
     }
 
-    patchPluginXml {
-        version = properties("pluginVersion")
-        sinceBuild = properties("pluginSinceBuild")
-        untilBuild = properties("pluginUntilBuild")
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-//        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-//            val start = "<!-- Plugin description -->"
-//            val end = "<!-- Plugin description end -->"
-//
-//            with (it.lines()) {
-//                if (!containsAll(listOf(start, end))) {
-//                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-//                }
-//                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-//            }
-//        }
-
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        }
-    }
 
     // Configure UI tests plugin
     // Read more: https://github.com/JetBrains/intellij-ui-test-robot
