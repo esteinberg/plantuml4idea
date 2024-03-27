8fa96b4ed1b126cd5a0644e3173bfa0ac56a67bb
Vojtěch Krása <vojta.krasa@gmail.com>
2024-03-27 21:53:16 +0100

    Fixes: Duplicate line action moves caret to line begining

--- src/main/java/org/plantuml/idea/grammar/psi/PumlNamedElement.java
+++ src/main/java/org/plantuml/idea/grammar/psi/PumlNamedElement.java
@@ -1,7 +1,7 @@
 package org.plantuml.idea.grammar.psi;
 
-import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.psi.PsiNamedElement;
 
-public interface PumlNamedElement extends PsiNameIdentifierOwner {
+public interface PumlNamedElement extends PsiNamedElement {
 
 }
